43fefcb1750b15ded52300fbf6b8e4ba7b602d45
bwilm <32461222+bwilm@users.noreply.github.com>
2018-01-24 14:36:47 -0500

    new

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kcrazeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the kcrazeApp
+ */
+angular.module('kcrazeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kcrazeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the kcrazeApp
+ */
+angular.module('kcrazeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
